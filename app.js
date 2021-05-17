a9cc95c96eaae79bd5abfb2526bbcd400158bba0
San4o1313 <56685642+San4o1313@users.noreply.github.com>
2021-05-17 12:50:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const linksEl = document.querySelector(".links");
+const containerEl = document.getElementById("container");
+const menuEl = document.getElementById("menu");
+const containerNavEl = document.getElementById("nav-container");
+
+const linksCopyEl = linksEl.cloneNode(true);
+linksCopyEl.classList.add ("bottom-menu");
+linksCopyEl.classList.remove ("links");
+
+containerEl.append(linksCopyEl);
+
+let menuIsOpen  = false;
+let hightNav = containerNavEl.clientHeight;
+let hightCont = containerEl.clientHeight;
+
+containerEl.style.height = hightNav+"px";
+
+menuEl.addEventListener("click", () => {
+    if (!menuIsOpen) {
+        containerEl.style.height = hightCont+"px";
+        menuIsOpen = true;
+        return;
+    }
+    containerEl.style.height = hightNav+"px";
+    menuIsOpen = false;
+});
+
+window.addEventListener("resize", () => {
+    if(document.body.clientWidth > 800) {
+        containerEl.style.height = hightNav+"px";
+        menuIsOpen = false;
+    }
+})
